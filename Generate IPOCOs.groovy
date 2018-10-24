329df173b2805d5a692c29272b4955f3cd40f413
André Leblanc <a.leblanc@live.com>
2018-10-24 16:00:27 -0400

    Generate an Interface file (#3)

---
+++ Generate IPOCOs.groovy
@@ -0,0 +1,59 @@
+import com.intellij.database.model.DasTable
+import com.intellij.database.model.ObjectKind
+import com.intellij.database.util.Case
+import com.intellij.database.util.DasUtil
+
+typeMapping = [
+        (~/(?i)^bit$/)                                    : "bool",
+        (~/(?i)^tinyint$/)                                : "byte",
+        (~/(?i)^uniqueidentifier|uuid$/)                  : "Guid",
+        (~/(?i)^int|integer$/)                            : "int",
+        (~/(?i)^bigint$/)                                 : "long",
+        (~/(?i)^varbinary|image$/)                        : "byte[]",
+        (~/(?i)^double|float|real$/)                      : "double",
+        (~/(?i)^decimal|money|numeric|smallmoney$/)       : "decimal",
+        (~/(?i)^datetimeoffset$/)                         : "DateTimeOffset",
+        (~/(?i)^datetime|datetime2|timestamp|date|time$/) : "DateTime",
+        (~/(?i)^char$/)                                   : "char",
+]
+
+notNullableTypes = [ "string", "byte[]" ]
+
+FILES.chooseDirectoryAndSave("Choose directory", "Choose where to store generated files") { dir ->
+    SELECTION.filter { it instanceof DasTable && it.getKind() == ObjectKind.TABLE }.each { generate(it, dir) }
+}
+
+def generate(table, dir) {
+    def className = csharpName(table.getName())
+    def fields = calcFields(table)
+    new File(dir, "I" + className + ".cs").withPrintWriter { out -> generate(out, className, fields) }
+}
+
+def generate(out, className, fields) {
+    out.println "using System;"
+    out.println ""
+    out.println "public interface I$className"
+    out.println "{"
+
+    fields.each() {
+        out.println "    ${it.type} ${it.name} { get; set; }"
+    }
+    out.println "}"
+}
+
+def calcFields(table) {
+    DasUtil.getColumns(table).reduce([]) { fields, col ->
+        def spec = Case.LOWER.apply(col.getDataType().getSpecification())
+        def typeStr = typeMapping.find { p, t -> p.matcher(spec).find() }?.value ?: "string"
+        def nullable = col.isNotNull() || typeStr in notNullableTypes ? "" : "?"
+        fields += [[
+                           name : csharpName(col.getName()),
+                           type : typeStr + nullable]]
+    }
+}
+
+def csharpName(str) {
+    com.intellij.psi.codeStyle.NameUtil.splitNameIntoWords(str)
+            .collect { Case.LOWER.apply(it).capitalize() }
+            .join("")
+}
